2ce88d2d7abbba385775af28c090d0fdb6d6b3ce
Koichi.Shimomura <koichi.shimomura@professionalmedia.jp>
2020-03-30 18:24:25 +0900

    3-3 テンプレートの基礎3 if文とリンクの作成

--- src/AppBundle/Controller/ConcertController.php
+++ src/AppBundle/Controller/ConcertController.php
@@ -16,27 +16,32 @@ class ConcertController extends Controller
             array(
                 'date'  => '2015年5月3日',
                 'time'  => '14:00',
-                'place' => '東京文化会館（満席）'
+                'place' => '東京文化会館',
+                'available' => false,
             ),
             array(
                 'date'  => '2015年7月12日',
                 'time'  => '14:00',
-                'place' => '鎌倉芸術館'
+                'place' => '鎌倉芸術館',
+                'available' => true,
             ),
             array(
                 'date'  => '2015年9月20日',
                 'time'  => '15:00',
-                'place' => '横浜みなとみらいホール'
+                'place' => '横浜みなとみらいホール',
+                'available' => true,
             ),
             array(
                 'date'  => '2015年11月8日',
                 'time'  => '15:00',
-                'place' => 'よこすか芸術劇場（満席）'
+                'place' => 'よこすか芸術劇場',
+                'available' => false,
             ),
             array(
                 'date'  => '2016年1月10日',
                 'time'  => '14:00',
-                'place' => '渋谷公開堂'
+                'place' => '渋谷公開堂',
+                'available' => true,
             ),
         );
 
